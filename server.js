e3e9c88dd78f38fbacaeaf945b2f14aa1e76d8a1
sharedordaz <sharedordaz@live.com.mx>
2022-06-16 19:22:13 -0500

    started working server

---
+++ server.js
@@ -0,0 +1,10 @@
+//importing http server
+const http = require("http");
+const port = 8000;
+
+
+http.createServer(function(request, response){
+    response.writeHead(200,{'Content-Type': 'text/html'}); //Create a header (different than HTML header), that is made to give information about the information we are sending by response of the HTTP request
+    response.write('hola mundo'); 
+    response.end(); //End the response progress
+}).listen(port);
